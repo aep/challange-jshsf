afd7b3b58776527050d3d09d6106b14fee1bafc3
Arvid E. Picciani <aep@exys.org>
2022-05-23 15:42:45 +0200

    init

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <err.h>
+#include <string.h>
+
+char response[] = "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+"<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
+"<style>body { background-color: #111 }"
+"h1 { font-size:4cm; text-align: center; color: black;"
+" text-shadow: 0 0 2mm red}</style></head>"
+"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+
+char b[100];
+
+void serve(int *fd) {
+    printf("got connection\n");
+
+    for (;;) {
+        read(fd, b, strlen(b));
+        if (strstr(b, "\r\n\r\n")) {
+            break;
+        }
+    }
+
+    write(fd, response, sizeof(response));
+}
+
+int main()
+{
+    struct sockaddr_in svr_addr, cli_addr;
+    socklen_t sin_len = sizeof(cli_addr);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        err(1, "can't open socket");
+    }
+
+    int one = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, one, sizeof(int));
+
+    int port = 8080;
+    svr_addr.sin_family = AF_INET;
+    svr_addr.sin_addr.s_addr = INADDR_ANY;
+    svr_addr.sin_port = htons(port);
+
+    if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+        close(sock);
+        err(1, "Can't bind");
+    }
+
+    listen(sock, 5);
+    while (1) {
+        int fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+        serve(fd);
+    }
+}
+
